d0277fbf0c7ae8aa6289467f3af773136bfbb58b
Ivan Tsurkan <curkanivan493@gmail.com>
2024-01-20 00:41:24 +0100

    another try

--- app/getUsers.js
+++ app/getUsers.js
@@ -1,10 +1,8 @@
-import { cache } from "react"
-
-const getUsers = cache(async () => {
-  const response = await fetch('https://randomuser.me/api/?results=7', { credentials: 'include', cache: 'force-cache' })
+const getUsers = async () => {
+  const response = await fetch('https://randomuser.me/api/?results=7', { credentials: 'include' })
   const parsedRes = await response.json()
 
   return parsedRes.results
-})
+}
 
 export { getUsers }
